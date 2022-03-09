05432a8165f8ad78b9358cd19f7b592af3dacefa
Matias iatrino <matiasiatrino@gmail.com>
2022-03-09 15:23:40 -0300

    Creo el archivo SweetAlert.js y le agrego estilos

---
+++ SweetAlert.js
@@ -0,0 +1,7 @@
+Swal.fire({
+    title: '¡Bienvenido!',
+    icon: 'info',
+    html: `<b class="alert-text">Este quiz consta de 10 preguntas, ¿Quieres poner a prueba tus conocimientos de Marvel?
+           ¡Pues adelante, mucha suerte!</b>`,
+    padding: 20
+});
