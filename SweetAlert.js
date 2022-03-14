45273370ce41a922d59012cf218769d5c9de2cde
Matias iatrino <matiasiatrino@gmail.com>
2022-03-13 23:36:57 -0300

    Agrego el archivo question.json de donde se consumen las preguntas de la aplicación

--- SweetAlert.js
+++ SweetAlert.js
@@ -1,7 +1,7 @@
 Swal.fire({
     title: '¡Bienvenido!',
     icon: 'info',
-    html: `<b class="alert-text">Este quiz consta de 10 preguntas, ¿Quieres poner a prueba tus conocimientos de Marvel?
+    html: `<b class="alert-text">Este quiz consta de 15 preguntas, ¿Quieres poner a prueba tus conocimientos del Universo Cinematrográfico de Marvel?
            ¡Pues adelante, mucha suerte!</b>`,
     padding: 20
 });
--- scripts/game.js
+++ scripts/game.js
@@ -10,91 +10,21 @@ let acceptingAnswers= false;
 let score = 0;
 let questionCounter=0;
 let availableQuestions=[];
-let questions=[
-    {
-        question:"¿En qué año se estrenó la primera película de Iron Man, que lanzó el Marvel Cinematic Universe?",
-        choice1: "2005",
-        choice2: "2006",
-        choice3: "2008",
-        choice4: "2010",
-        answer:  3
-      },
-      {
-          question:"¿Cómo se llama el martillo de Thor?",
-          choice1: "Vanir",
-          choice2: "Mjolnir",
-          choice3: "Aesir",
-          choice4: "Jonathan",
-        answer:  2
-        },
-        {
-        question:"En The Incredible Hulk, ¿qué le dice Tony a Thaddeus Ross al final de la película?",
-        choice1: "Que quiere estudiar a Hulk",
-        choice2: "Que él sabe sobre SHIELD",
-        choice3: "Que están formando un equipo",
-        choice4: "Que Thaddeus le debe dinero",
-        answer:  3
-        },
-        {
-        question: "¿De qué está hecho el escudo del Capitán América?",
-        choice1: "Adamantium",
-        choice2: "Vibranio",
-         choice3: "Prometeo",
-        choice4: "Carbonadio",
-        answer:  2
-        },
-        {
-        question:" Los Flerkens son una raza de alienígenas extremadamente peligrososas ¿qué se parece a que animal?",
-        choice1: "Gatos",
-        choice2: "Patos",
-        choice3: "Reptiles",
-        choice4: "Mapaches",
-        answer:  1
-        },
-        {
-        question: "Antes de convertirse en Visión, ¿cómo se llama el mayordomo de inteligencia artificial de Iron Man?",
-        choice1:"HOMERO",
-        choice2:"JARVIS",
-        choice3:"ALFRED",
-        choice4:"MARVIN",
-        answer:  2
-        },
-        {
-        question:"¿Cuál es el verdadero nombre de la Pantera Negra?",
-        choice1: "T'Challa",
-         choice2: "M'Baku",
-        choice3: "N'Jadaka",
-        choice4: "N'Jobu",
-        answer:  1
-        },
-        {
-        question:"¿Cuál es la raza alienígena que Loki envía para invadir la Tierra en The Avengers?",
-        choice1:"Chitauris",
-        choice2:"Skrulls",
-        choice3:"Krees",
-      choice4:"Flerkens",
-        answer: 1
-        },
-        {
-        question:"¿Quién fue el último titular de la Gema del Espacio antes de que Thanos lo reclame para su Guantelete?",
-        choice1: "Thor",
-        choice2: "Loki",
-        choice3: "El coleccionista",
-        choice4: "Tony Stark",
-        answer:  2
-        },
-        {
-         question:"¿Qué nombre falso usa Natasha cuando conoce a Tony por primera vez?",
-        choice1: "Natalie Rushman",
-        choice2: "Natalia Romanoff",
-        choice3: "Nicole Rohan",
-         choice4: "Naya Rabe",
-      answer:  1
-        }
-]
+let questions=[];
+
+fetch('questions.json')
+    .then(res=>{
+        return res.json();
+    })
+    .then(loadedQuestions=>{
+        console.log(loadedQuestions);
+        questions=loadedQuestions;
+        starGame();
+    })
+
 //CONSTANTS 
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS=10;
+const MAX_QUESTIONS=15;
 
 starGame =()=>{
     questionCounter=0;
@@ -107,7 +37,7 @@ getNewQuestion = ()=>{
     if(availableQuestions.length===0||questionCounter>=MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore',score);
         //go to the End Page
-        return window.location.assign('../pages/end.html');
+        return window.location.assign('./end.html');
     }
 
     questionCounter++;
@@ -155,4 +85,4 @@ incrementScore = num =>{
     score+=num;
     scoreText.innerText = score
 }
-starGame();
+
